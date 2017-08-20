72175f08c5c4dbccfc03594c023279b7d0b14833
Serkan Sahin <xserkanx@hotmail.co.uk>
2017-08-20 18:48:46 +0100

    Initial commit

---
+++ Sentrio/CryptoWorks.cs
@@ -0,0 +1,484 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Sentrio
+{
+    public class CryptoWorks
+    {
+        #region Properties
+        // Preconfigured password key derivation parameters
+        // public const string PasswordChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%^&*()_+-=[]{}'#@~,.<>/? ";
+        private static Aes AES = new AesCryptoServiceProvider() { Mode = CipherMode.CBC, Padding = PaddingMode.PKCS7 };
+        // public const CipherMode CM = CipherMode.CBC;
+        // public const PaddingMode PM = PaddingMode.PKCS7;
+        // public const int KeySize = 256;
+        // public const int SaltSize = KeySize / 8;
+        public int iterations = 10000;
+
+        // Software properties
+        public bool AutoEncrypt = true;
+        public bool AutoDecrypt = true;
+        #endregion
+
+        #region Constructors
+        public CryptoWorks()
+        {
+
+        }
+        #endregion
+
+        #region Utilities
+        /// <summary>
+        /// Converts the given byte array to "hexadecimal" string.
+        /// </summary>
+        /// <param name="ba">The byte array to convert.</param>
+        /// <returns>String in hexadecimal format.</returns>
+        public string ByteArrayToString(byte[] ba)
+        {
+            if (ba != null && ba.Length > 0)
+            {
+                StringBuilder hex = new StringBuilder(ba.Length * 2);
+                foreach (byte b in ba)
+                {
+                    hex.AppendFormat("{0:x2}", b);
+                }
+                return hex.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Converts the given "hexadecimal" string to byte array.
+        /// </summary>
+        /// <param name="hex">The hexadecimal string to convert.</param>
+        /// <returns>Byte array derived from the hexadecimal string.</returns>
+        public byte[] StringToByteArray(string hex)
+        {
+            if (string.IsNullOrWhiteSpace(hex) == false)
+            {
+                int NumberChars = hex.Length;
+                byte[] bytes = new byte[NumberChars / 2];
+                for (int i = 0; i < NumberChars; i += 2)
+                    bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+                return bytes;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Generate a cryptographically strong random key using AES.
+        /// </summary>
+        /// <returns>Byte array of the generated key.</returns>
+        public byte[] GenerateRandomKey()
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.GenerateKey();
+                return aes.Key;
+            }
+        }
+
+        /// <summary>
+        /// Generate a cryptographically strong random salt using RNG based on given salt size.
+        /// </summary>
+        /// <param name="saltSize">The size of salt.</param>
+        /// <returns>Byte array of the generated salt.</returns>
+        public byte[] GenerateRandomSalt(int saltSize)
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] random = new byte[saltSize];
+                rng.GetBytes(random);
+                return random;
+            }
+        }
+
+        /// <summary>
+        /// Compare two byte arrays and return true if both are equal.
+        /// </summary>
+        /// <param name="a1">First byte array to compare from.</param>
+        /// <param name="a2">Second byte array to compare with.</param>
+        /// <returns></returns>
+        public bool CompareByteArrays(byte[] a1, byte[] a2)
+        {
+            if (a1 != null && a1.Length > 0 && a2 != null && a2.Length > 0)
+            {
+                if (a1.Length != a2.Length)
+                {
+                    return false;
+                }
+
+                for (int i = 0; i < a1.Length; i++)
+                {
+                    if (a1[i] != a2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Hashes a given file from file path.
+        /// </summary>
+        /// <param name="filePathIn">The file to hash in full file path.</param>
+        /// <returns>An array of hashes in order specified.</returns>
+        public string[] Hash(string filePathIn)
+        {
+            if (String.IsNullOrEmpty(filePathIn)) return new string[0];
+
+            List<string> list = new List<string>();
+
+            #region MD5
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = new FileStream(filePathIn, FileMode.Open))
+                {
+                    list.Add("MD5: " + ByteArrayToString(md5.ComputeHash(stream)).ToUpper());
+                }
+            }
+            #endregion
+
+            #region SHA1
+            using (var sha1 = SHA1.Create())
+            {
+                using (var stream = new FileStream(filePathIn, FileMode.Open))
+                {
+                    list.Add("SHA1: " + ByteArrayToString(sha1.ComputeHash(stream)).ToUpper());
+                }
+            }
+            #endregion
+
+            #region SHA256
+            using (var sha256 = SHA256.Create())
+            {
+                using (var stream = new FileStream(filePathIn, FileMode.Open))
+                {
+                    list.Add("SHA256: " + ByteArrayToString(sha256.ComputeHash(stream)).ToUpper());
+                }
+            }
+            #endregion
+
+            #region SHA512
+            using (var sha512 = SHA512.Create())
+            {
+                using (var stream = new FileStream(filePathIn, FileMode.Open))
+                {
+                    list.Add("SHA512: " + ByteArrayToString(sha512.ComputeHash(stream)).ToUpper());
+                }
+            }
+            #endregion
+
+            return list.ToArray();
+        }
+        #endregion
+
+        #region File
+        /// <summary>
+        /// Encrypt a file from source file path to destination file path, using a password.
+        /// </summary>
+        /// <param name="FilePathIn">The path of the file to encrypt.</param>
+        /// <param name="FilePathOut">The path to save the encrypted file.</param>
+        /// <param name="password">The password to encrypt the file.</param>
+        public void Encrypt(string FilePathIn, string FilePathOut, string password)
+        {
+            // Open the source file
+            using (var FileIn = new FileStream(FilePathIn, FileMode.Open))
+            // Create the destination file
+            using (var FileOut = new FileStream(FilePathOut, FileMode.Create))
+            // Derive key using password, salt (length of IV) and iterations
+            using (var RFC = new Rfc2898DeriveBytes(password, AES.IV.Length, iterations))
+            {
+                // Set AES key from RFC key derivation
+                AES.Key = RFC.GetBytes(AES.KeySize / 8);
+
+                // Encrypt file and get the stream
+                using (var CryptoStream = Crypto(FileIn, AES, RFC, true))
+                {
+                    byte[] data = CryptoStream.ToArray();   // Get encrypted content
+                    FileOut.Write(data, 0, data.Length);    // Write to destination file
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decrypt a file from source file path to destination file path, using a password.
+        /// </summary>
+        /// <param name="FilePathIn">The path of the file to decrypt.</param>
+        /// <param name="FilePathOut">The path to save the decrypted file.</param>
+        /// <param name="password">The password to decrypt the file.</param>
+        public void Decrypt(string FilePathIn, string FilePathOut, string password)
+        {
+            // Open the source file
+            using (var FileIn = new FileStream(FilePathIn, FileMode.Open))
+            // Create the destination file
+            using (var FileOut = new FileStream(FilePathOut, FileMode.Create))
+            {
+                // Hold bytes for iteration count, using the size from current byte
+                byte[] IterationsBytes = new byte[FileIn.ReadByte()];
+                // Fill the array with bytes from file, of array length
+                FileIn.Read(IterationsBytes, 0, IterationsBytes.Length);
+                // Parse iteration count from the array
+                int iterations = int.Parse(Encoding.ASCII.GetString(IterationsBytes));
+
+                // Hold bytes for salt, using the size from current byte
+                byte[] salt = new byte[(FileIn.ReadByte())];
+                // Fill the array with bytes from file, in the current position and of array length
+                FileIn.Read(salt, 0, salt.Length);
+
+                // Hold bytes for IV, using the size from current byte
+                byte[] IV = new byte[FileIn.ReadByte()];
+                // Fill the array with bytes from file, in the current position and of array length
+                FileIn.Read(IV, 0, IV.Length);
+
+                // Derive key using password, retrieved salt and retrieved iterations
+                using (var RFC = new Rfc2898DeriveBytes(password, salt, iterations))
+                {
+                    // Set AES key from RFC key derivation
+                    AES.Key = RFC.GetBytes(AES.KeySize / 8);
+                    // Set AES IV from IV retrieved in file
+                    AES.IV = IV;
+
+
+                    // Decrypt file and get the stream
+                    using (var CryptoStream = Crypto(FileIn, AES, RFC, false))
+                    {
+                        // Maybe use copyto?
+                        byte[] data = CryptoStream.ToArray();   // Get decrypted content
+                        FileOut.Write(data, 0, data.Length);    // Write to destination file
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Text
+        /// <summary>
+        /// Encrypts a string message using given password.
+        /// </summary>
+        /// <param name="message">
+        /// The message to encrypt.
+        /// </param>
+        /// <param name="password">
+        /// The password to encrypt the message with.
+        /// </param>
+        /// <returns>
+        /// The encrypted message from the supplied message and password.
+        /// </returns>
+        public byte[] Encrypt(byte[] message, byte[] password)
+        {
+            using (var MessageIn = new MemoryStream(message))
+            using (var RFC = new Rfc2898DeriveBytes(password, GenerateRandomSalt(AES.IV.Length), iterations))
+            {
+                AES.Key = RFC.GetBytes(AES.KeySize / 8);
+
+                using (var MessageOut = Crypto(MessageIn, AES, RFC, true))
+                {
+                    return MessageOut.ToArray();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decrypts a string message using given password.
+        /// </summary>
+        /// <param name="message">
+        /// The message to decrypt.
+        /// </param>
+        /// <param name="password">
+        /// The password to decrypt the message with.
+        /// </param>
+        /// <returns>
+        /// The decrypted message from the supplied message and password.
+        /// </returns>
+        public byte[] Decrypt(byte[] message, byte[] password)
+        {
+            using (var MessageIn = new MemoryStream(message))
+            {
+                byte[] IterationsBytes = new byte[MessageIn.ReadByte()];
+                MessageIn.Read(IterationsBytes, 0, IterationsBytes.Length);
+                int iterations = int.Parse(Encoding.ASCII.GetString(IterationsBytes));
+                byte[] salt = new byte[(MessageIn.ReadByte())];
+                MessageIn.Read(salt, 0, salt.Length);
+                byte[] IV = new byte[MessageIn.ReadByte()];
+                MessageIn.Read(IV, 0, IV.Length);
+
+                using (var RFC = new Rfc2898DeriveBytes(password, salt, iterations))
+                {
+                    AES.Key = RFC.GetBytes(AES.KeySize / 8);
+                    AES.IV = IV;
+
+                    using (var MessageOut = Crypto(MessageIn, AES, RFC, false))
+                    {
+                        return MessageOut.ToArray();
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Operations
+        #region Old
+        /*
+        private Stream Encrypt(Stream StreamIn, string password)
+        {
+            // Create a custom Aes object that has predefined parameters
+            using (var sk = aes())
+            using (var rfc = new Rfc2898DeriveBytes(password, sk.KeySize / 8))
+            {
+                #region Salt
+                // Hold salt bytes that have been generated by the Rfc2898 class
+                byte[] salt = rfc.Salt;
+                // Hold the length of the salt byte array for use in decryption
+                byte saltLength = Convert.ToByte(salt.Length);
+                #endregion
+
+                #region IV
+                // Hold the IV bytes that have been automatically generated when initialising the Aes class
+                byte[] iv = sk.IV;
+                // Hold the length of the IV byte array for use in decryption
+                byte IvLength = Convert.ToByte(iv.Length);
+                #endregion
+
+                #region Key Derivation
+                // Derive key byte array from the given password string, salt and iterations, based on the
+                // key size used
+                byte[] key = rfc.GetBytes(sk.KeySize / 8);
+                // Set the key of the custom Aes object to the derived key
+                sk.Key = key;
+                #endregion
+
+                using (var StreamOut = new MemoryStream())
+                {
+                    using (var transform = sk.CreateEncryptor())
+                    using (var cs = new CryptoStream(StreamOut, transform, CryptoStreamMode.Write))
+                    {
+                        #region Headers
+                        // Write the salt length, salt, IV length and IV to the start of the
+                        // file, in their respective order, then finalise with the encrypted content
+                        StreamOut.WriteByte(saltLength);            // Salt Length
+                        StreamOut.Write(salt, 0, salt.Length);      // Salt
+                        StreamOut.WriteByte(IvLength);              // IV Length
+                        StreamOut.Write(iv, 0, iv.Length);          // IV
+                        StreamOut.WriteByte((byte)StreamIn.Length); // Data length
+                        #endregion
+
+                        #region Data
+                        // Write the decrypted stream to file and flush final block.
+                        // This is not recommended as it does not give steps where progress
+                        // can be tracked.
+                        StreamIn.CopyTo(cs);
+
+                        cs.FlushFinalBlock();
+                        #endregion
+                    }
+
+                    // Return encrypt output stream
+                    return new MemoryStream(StreamOut.ToArray());
+                }
+            }
+        }
+
+        private Stream Decrypt(Stream StreamIn, string password)
+        {
+            using (var sk = aes())
+            {
+                #region Salt
+                // Read from first byte which holds the salt length
+                int saltLength = StreamIn.ReadByte();
+                // Create a salt byte array of size saltLength that was read previously
+                byte[] salt = new byte[saltLength];
+                // Fill the salt byte array with bytes of given length
+                StreamIn.Read(salt, 0, salt.Length);
+                #endregion
+
+                #region IV
+                // Read from current byte which holds the IV length
+                int IvLength = StreamIn.ReadByte();
+                // Create an IV byte array of size ivLength that was read previously
+                byte[] iv = new byte[IvLength];
+                // Fill the IV byte array with bytes of given length
+                StreamIn.Read(iv, 0, iv.Length);
+                #endregion
+
+                #region Data Information
+                // Read from current byte which holds the length of data
+                int DataLength = StreamIn.ReadByte();
+                #endregion
+
+                // Derive key from the password and retrieved salt from file
+                using (var rfc = new Rfc2898DeriveBytes(password, salt))
+                {
+                    #region Key Derivation
+                    // Derive key byte array from the given password string, salt and iterations, based on the
+                    // key size used
+                    byte[] key = rfc.GetBytes(sk.KeySize / 8);
+                    #endregion
+
+                    sk.Key = key;
+                    sk.IV = iv;
+
+                    using (var StreamOut = new MemoryStream())
+                    {
+                        using (var transform = sk.CreateDecryptor())
+                        using (var cs = new CryptoStream(StreamOut, transform, CryptoStreamMode.Write))
+                        {
+                            #region Data
+                            // Write the decrypted stream to file and flush final block.
+                            // This is not recommended as it does not give steps where progress
+                            // can be tracked.
+                            StreamIn.CopyTo(cs);
+                            #endregion
+                        }
+                        return new MemoryStream(StreamOut.ToArray());
+                    }
+                }
+            }
+            */
+        #endregion
+
+        #region New
+        private MemoryStream Crypto(Stream MessageIn, Aes AES, Rfc2898DeriveBytes RFC, bool encrypt)
+        {
+            using (var MessageOut = new MemoryStream())
+            using (var transform = encrypt ? AES.CreateEncryptor() : AES.CreateDecryptor())
+            using (var CS = new CryptoStream(MessageOut, transform, CryptoStreamMode.Write))
+            {
+                if (encrypt)
+                {
+                    byte[] IterationBytes = Encoding.ASCII.GetBytes(RFC.IterationCount.ToString());
+                    MessageOut.WriteByte(Convert.ToByte(IterationBytes.Length));    // Iteration Length
+                    MessageOut.Write(IterationBytes, 0, IterationBytes.Length);     // Iteration
+                    MessageOut.WriteByte(Convert.ToByte(RFC.Salt.Length));          // Salt Length
+                    MessageOut.Write(RFC.Salt, 0, RFC.Salt.Length);                 // Salt
+                    MessageOut.WriteByte(Convert.ToByte(AES.IV.Length));            // IV Length
+                    MessageOut.Write(AES.IV, 0, AES.IV.Length);                     // IV
+
+                    MessageIn.CopyTo(CS);                                           // Ciphertext
+                    return MessageOut;
+                }
+                else
+                {
+                    MessageIn.CopyTo(CS);
+                    return MessageOut;
+                }
+            }
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ SentrioTests/CryptoWorksTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Sentrio.Tests
+{
+    [TestClass()]
+    public class CryptoWorksTests
+    {
+        /// <summary>
+        /// Tests if <see cref="CryptoWorks.CompareByteArrays(byte[], byte[])"/> works correctly.
+        /// Uses two test cases where two arrays equal and two arrays do not equal each other.
+        /// </summary>
+        [TestMethod()]
+        public void CompareByteArraysTest()
+        {
+            byte[] a1 = { 0x82, 0x44, 0xF4, 0x15, 0x77, 0x05 };
+            byte[] a2 = { 0x9B, 0xE2, 0xAA, 0x3A, 0x6B, 0x82 };
+            bool r1 = new CryptoWorks().CompareByteArrays(a1, a2);
+
+            byte[] a3 = { 0x78, 0x8D, 0x82, 0xFD, 0x34, 0xFE };
+            byte[] a4 = { 0x78, 0x8D, 0x82, 0xFD, 0x34, 0xFE };
+            bool r2 = new CryptoWorks().CompareByteArrays(a3, a4);
+
+            Assert.IsTrue(!r1 && r2);
+        }
+
+        /// <summary>
+        /// Tests if <see cref="CryptoWorks.ByteArrayToString(byte[])"/> works correctly.
+        /// </summary>
+        [TestMethod()]
+        public void ByteArrayToStringTest()
+        {
+            byte[] a = { 0x76, 0x69, 0xC0, 0xDB, 0x5F, 0x3E };
+            string expected = "7669c0db5f3e";
+            string actual = new CryptoWorks().ByteArrayToString(a);
+
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+
+        /// <summary>
+        /// Tests if <see cref="CryptoWorks.StringToByteArray(string)"/> works correctly.
+        /// Does not depend on <see cref="CryptoWorks.CompareByteArrays(byte[], byte[])"/>.
+        /// </summary>
+        [TestMethod()]
+        public void StringToByteArrayTest()
+        {
+            string hex = "813C18D33137";
+            byte[] expected = { 0x81, 0x3C, 0x18, 0xD3, 0x31, 0x37 };
+            byte[] actual = new CryptoWorks().StringToByteArray(hex);
+
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+    }
+}
